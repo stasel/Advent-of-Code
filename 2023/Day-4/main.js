6a27b089a906f4536becf2825508b24c86e55148
stasel <2033301+stasel@users.noreply.github.com>
2023-12-15 14:26:37 +0100

    Added Day 4 part 2

--- 2023/Day-4/main.js
+++ 2023/Day-4/main.js
@@ -8,23 +8,47 @@ const parseScratchCard = (stringLine) => {
     const [header, data] = stringLine.split(':');
     const cardNumber = Number.parseInt(header.replace("Card ",""));
     const [winningNumbers, givenNumbers] = data.split(' | ').map(parseNumberData);
-    return { number: cardNumber, winningNumbers, givenNumbers };
+    return { 
+        number: cardNumber, 
+        winningNumbers, givenNumbers,
+        numberOfMatches: winningNumbers.intersect(givenNumbers).size 
+    };
+};
+
+const calculateTotalCardsWon = (card, allCards, cache) => {
+    if(cache[card.number]) {
+        return cache[card.number];
+    }
+    let cardWonCount = 0;
+    cardWonCount += card.numberOfMatches;
+    const wonCards = allCards.slice(card.number, card.number + card.numberOfMatches);
+    for (const newCard of wonCards) {
+        cardWonCount += calculateTotalCardsWon(newCard, allCards, cache);
+    }
+    cache[card.number] = cardWonCount;
+    return cardWonCount;
 };
 
 const part1 = async () => {
     const file = await utils.loadFile('./input');
     const scratchCards = file.split('\n').map(parseScratchCard);
     const solution = scratchCards
-        .map(card => card.winningNumbers.intersect(card.givenNumbers).size)
-        .filter(numberOfMatches => numberOfMatches > 0)
-        .map(numberOfMatches => 2 ** (numberOfMatches-1))
+        .filter(card => card.numberOfMatches > 0)
+        .map(card => 2 ** (card.numberOfMatches - 1))
         .sum();
     
     console.log("Part 1 solution: ", solution); 
 };
 
 const part2 = async () => {
-    console.log("Part 2 solution: ", 0);
+    const file = await utils.loadFile('./input');
+    let cache = {};
+    const scratchCards = file.split('\n').map(parseScratchCard)
+    const solution = scratchCards
+        .map(card => calculateTotalCardsWon(card, scratchCards, cache))
+        .sum() + scratchCards.length;
+
+    console.log("Part 2 solution: ", solution);
 };
 
 await part1();
