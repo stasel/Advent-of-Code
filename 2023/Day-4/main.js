5bcb33068eb90a12ee62472cb7a56e1590e5d6aa
stasel <2033301+stasel@users.noreply.github.com>
2023-12-15 13:22:55 +0100

    Day 4 part 1

--- 2023/Day-4/main.js
+++ 2023/Day-4/main.js
@@ -1,8 +1,26 @@
 import * as utils from '../../utils/index.js'
 
+const parseNumberData = (numberData) => {
+    return new Set(numberData.trim().split(/ +/).map(el => Number.parseInt(el)));
+};
+
+const parseScratchCard = (stringLine) => {
+    const [header, data] = stringLine.split(':');
+    const cardNumber = Number.parseInt(header.replace("Card ",""));
+    const [winningNumbers, givenNumbers] = data.split(' | ').map(parseNumberData);
+    return { number: cardNumber, winningNumbers, givenNumbers };
+};
+
 const part1 = async () => {
     const file = await utils.loadFile('./input');
-    console.log("Part 1 solution: ", 0); 
+    const scratchCards = file.split('\n').map(parseScratchCard);
+    const solution = scratchCards
+        .map(card => card.winningNumbers.intersect(card.givenNumbers).size)
+        .filter(numberOfMatches => numberOfMatches > 0)
+        .map(numberOfMatches => 2 ** (numberOfMatches-1))
+        .sum();
+    
+    console.log("Part 1 solution: ", solution); 
 };
 
 const part2 = async () => {
