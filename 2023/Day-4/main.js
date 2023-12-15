667db73e4cb7577905a068957588ecb8c65ce328
stasel <2033301+stasel@users.noreply.github.com>
2023-12-15 13:02:18 +0100

    Day 4 template

---
+++ 2023/Day-4/main.js
@@ -0,0 +1,13 @@
+import * as utils from '../../utils/index.js'
+
+const part1 = async () => {
+    const file = await utils.loadFile('./input');
+    console.log("Part 1 solution: ", 0); 
+};
+
+const part2 = async () => {
+    console.log("Part 2 solution: ", 0);
+};
+
+await part1();
+await part2();
